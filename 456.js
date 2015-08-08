b89bfe10aacac9e80bb592beadb4af56f5ab0e47
wang3623246 <gcxabp83107@chacuo.net>
2015-08-08 19:42:38 +0800

    Update 456.js

--- 456.js
+++ 456.js
@@ -1,1 +1,1 @@
-<a href="http://www.sina.com.cn" target="_blank"><img src="http://mail.tom.com/images/img_090424/focu_img_479_20111013.jpg" border=0 /></a>
+document.writeln("<a href=\"http://www.sina.com.cn\" target=\"_blank\"><img src=\"http://mail.tom.com/images/img_090424/focu_img_479_20111013.jpg\" border=0 /></a>");
